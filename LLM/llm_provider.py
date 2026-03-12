d6274db856b7cdc7be30173471209de18c996d52
bibhanshuraj19 <bibhanshuraj19@gmail.com>
2026-03-12 14:38:31 +0530

    changed llm and temperature

--- LLM/llm_provider.py
+++ LLM/llm_provider.py
@@ -3,6 +3,6 @@ from deepgram.types.think_settings_v1provider import ThinkSettingsV1Provider_Ope
 
 def get_llm_settings(prompt: str):
     return ThinkSettingsV1(
-        provider=ThinkSettingsV1Provider_OpenAi(type="open_ai", model="gpt-4o-mini", temperature=0.7),
+        provider=ThinkSettingsV1Provider_OpenAi(type="open_ai", model="gpt-4o", temperature=0.2),
         prompt=prompt,
     )
